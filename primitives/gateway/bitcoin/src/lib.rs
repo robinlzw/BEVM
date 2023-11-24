7f8e8b01ac1a29afd4aff05997321f46d2b6fb82
icodezjb <icodezjb@users.noreply.github.com>
2023-11-24 11:16:51 +0800

    Add gateway primitives

---
+++ primitives/gateway/bitcoin/src/lib.rs
@@ -0,0 +1,17 @@
+// Copyright 2023 BEVM Project Authors. Licensed under GPL-3.0.
+
+//! Some primitives and utils about ChainX gateway bitcoin.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![deny(missing_docs)]
+#![allow(clippy::type_complexity)]
+
+mod detector;
+mod extractor;
+mod types;
+mod utils;
+
+pub use self::detector::BtcTxTypeDetector;
+pub use self::extractor::{AccountExtractor, OpReturnExtractor};
+pub use self::types::{BtcDepositInfo, BtcTxMetaType, BtcTxType, OpReturnAccount};
+pub use self::utils::*;
